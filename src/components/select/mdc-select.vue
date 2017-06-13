2f7d17e93d1da76d2329dfcc859fca774fd13781
Peter Lauck <griest024@gmail.com>
2017-06-12 21:00:09 -0400

    added component API skeleton and comments

--- src/components/select/mdc-select.vue
+++ src/components/select/mdc-select.vue
@@ -1,6 +1,12 @@
 <template>
-  <div class="mdc-select">
-    <span class="mdc-select__selected-text">{{ selectedText }}</span>
+  <!-- native select -->
+  <select class="mdc-select" v-if="isNative()">
+    <option default selected>{{ defaultText }}</option>
+  </select>
+
+  <!-- custom select -->
+  <div class="mdc-select" v-else>
+    <span class="mdc-select__selected-text">{{ defaultText }}</span>
   
     <slot/>
   </div>
@@ -13,8 +19,13 @@ export default {
   name: 'mdc-select',
   props: {
     selectedText: String,
+    disabled: Boolean,
     native: Boolean,
-    options: {
+    type: {
+      type: String,
+      default: 'custom'
+    },
+    items: {
       type: Array,
       default: [],
       validator (value) {
@@ -24,17 +35,97 @@ export default {
       }
     }
   },
+  methods: {
+    // component API methods
+
+    /**
+     * Analogous to `HTMLSelectElement.prototype.item`
+     * Returns the option at the specified index, or null if the index is out of bounds
+     *
+     * @param  {Number} index
+     * @return {HTMLElement?}
+     */
+    item (index) {
+
+    },
+
+    /**
+     * Analogous to `HTMLSelectElement.prototype.nameditem`
+     * Returns the options either whose id equals the given key, or whose name attribute equals the given key
+     * Returns null if no item with an id or name attribute of the specified key is found
+     *
+     * @param  {String} key
+     * @return {HTMLElement?}
+     */
+    nameditem (key) {
+
+    }
+  },
   computed: {
     classes () {
       return {
         ...this.rootClasses
       }
+    },
+    menu () {
+
+    },
+    isNative () {
+      return this.native || this.type === 'native'
+    },
+    // read-only component API properties
+
+    /**
+     * The id of the currently selected option
+     * If no id is present on the selected option, its textContent is used
+     *
+     * @return {String} Returns an empty string when no option is selected
+     */
+    value () {
+
+    },
+
+    /**
+     * An array of menu items comprising the select's options
+     *
+     * @return {HTMLElement[]}
+     */
+    options () {
+
+    },
+
+    /**
+     * A NodeList of either the currently selected option, or no elements if nothing is selected
+     *
+     * @return {HTMLElement[]}
+     */
+    selectedOptions () {
+
     }
   },
   data () {
     return {
       foundation: null,
-      rootClasses: {}
+      rootClasses: {},
+      selectedText: this.defaultText,
+      // writable component API properties
+
+      /**
+       * The index of the currently selected option
+       * Set to -1 if no option is currently selected
+       * Changing this property will update the select element
+       *
+       * @type {Number}
+       */
+      selectedIndex: -1,
+
+      /**
+       * Whether or not the component is disabled
+       * Settings this sets the disabled state on the component
+       *
+       * @type {Boolean}
+       */
+      isDisabled: this.disabled
     }
   },
   mounted () {
@@ -99,7 +190,7 @@ export default {
         return vm.menu.open
       },
       setSelectedTextContent (selectedTextContent) {
-        vm.selectedText_.textContent = selectedTextContent
+        vm.selectedText = selectedTextContent
       },
       getNumberOfOptions () {
         return vm.options.length
